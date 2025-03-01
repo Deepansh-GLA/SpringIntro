83d48c19f9c27cde58aa22ae30369542f9127fb4
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-03-01 23:41:40 +0530

    Pushing UC2

--- src/main/java/com/example/SpringIntro/SpringIntroApplication.java
+++ src/main/java/com/example/SpringIntro/SpringIntroApplication.java
@@ -1,13 +1,22 @@
 package com.example.SpringIntro;
 
+import components.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIntroApplication {
+	private static final Logger logger = LoggerFactory.getLogger(SpringIntroApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringIntroApplication.class, args);
+		logger.info("Starting Spring Boot Application...");
+		ApplicationContext context = SpringApplication.run(SpringIntroApplication.class, args);
+		logger.debug("Checking Context:{}",context.getBean(DemoBean.class));
+
+
 	}
 
 
